ed57c32a1f5dc330bf8c20c6734c5a528a3d1f41
moshirosang <tiw_tisadee@hotmail.com>
2020-08-20 01:42:16 +0700

    first commit

---
+++ src/Application.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        System.out.println("TIW");
+    }
+}
